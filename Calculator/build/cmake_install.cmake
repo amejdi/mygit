35440c2974929c0d1edd855d8f23edb325b6706d
MEJDI-PC\abder <abmejdi@gmail.com>
2019-03-04 11:53:02 -0800

    changed cmake
    intel compiler 18

--- Calculator/build/cmake_install.cmake
+++ Calculator/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/ESI_work/MyGit/Qt examples/TEST-2/Calculator
+# Install script for directory: D:/mygit/Calculator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/ESI_work/MyGit/Qt examples/TEST-2/Calculator/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/mygit/Calculator/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Calculator/buildwin.bat
+++ Calculator/buildwin.bat
@@ -1,5 +1,5 @@
 rmdir /S /Q build
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64"
+cmake .. -G "Visual Studio 15 2017 Win64" -T "Intel C++ Compiler 18.0"
 cd ..
